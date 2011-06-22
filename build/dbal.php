75ef938ce10098db7c19f3f147e134e50ac701b7
Alex <alexxemilov9@abv.bg>
2011-06-22 20:07:40 +0300

    empty results are not passed via obj

--- build/dbal.php
+++ build/dbal.php
@@ -8,7 +8,7 @@ class dbal
     
     function dbal($details = null)
     {
-        if($details)
+        if(is_array($details))
         {
             $this->dbal_connect($details);
         }
@@ -19,6 +19,7 @@ class dbal
         switch($details['dbtype'])
         {
             default:
+            case 'sql':
             case 'mysql':
                 $details['dbtype'] = 'mysql';
             break;
@@ -69,19 +70,23 @@ class dbal
         }
         
         $dbQuery = $this->dbal->prepare($sql);
-        $dbQuery->setFetchMode($fetchMode);
-        $dbQuery->execute($query);
-        
-        if(!method_exists($this->dbal->prepare($sql), $fetchType))
+        if(!! $dbQuery !== false)
         {
-            throw new PDOException("PDO dbal: There is no such PDO fetch type ({$fetchType}))");
+            $dbQuery->setFetchMode($fetchMode);
+            $dbQuery->execute($query);
+            
+            if(!method_exists($this->dbal->prepare($sql), $fetchType))
+            {
+                throw new PDOException("PDO dbal: There is no such PDO fetch type ({$fetchType}))");
+            }
+            
+            $fetchMethod = $fetchType | 'fetch';
+            $dbQuery = (!! $additional === true) ? array_merge($dbQuery->$fetchMethod(), array('__rows'  =>  $dbQuery->rowCount(), '__columns'  =>  $dbQuery->columnCount())) : $dbQuery->$fetchMethod();
+            $this->last_query = $dbQuery;
+            
+            return $dbQuery;
         }
-        
-        $fetchMethod = (isset($fetchType)) ? $fetchType : 'fetch';
-        $dbQuery = (!! $additional === true) ? array_merge($dbQuery->$fetchMethod(), array('__rows'  =>  $dbQuery->rowCount(), '__columns'  =>  $dbQuery->columnCount())) : $dbQuery->$fetchMethod();
-        $this->last_query = $dbQuery;
-        
-        return $dbQuery;
+        return false;
     }
     
     function query($sql, $fetchMode = PDO::FETCH_BOTH, $fetchType = 'fetch', $additional = false)
@@ -93,21 +98,25 @@ class dbal
         }
         
         $dbQuery = $this->dbal->query($sql);
-        $dbQuery->setFetchMode($fetchMode);
-        
-        if(!method_exists($this->dbal->query($sql), $fetchType))
+        if(!! $dbQuery !== false)
         {
-            throw new PDOException("PDO dbal: There is no such PDO fetch type ({$fetchType}))");
+            $dbQuery->setFetchMode($fetchMode);
+            
+            if(!method_exists($this->dbal->query($sql), $fetchType))
+            {
+                throw new PDOException("PDO dbal: There is no such PDO fetch type ({$fetchType}))");
+            }
+            
+            $fetchMethod = $fetchType | 'fetch';
+            $dbQuery = (!! $additional === true) ? array_merge($dbQuery->$fetchMethod(), array('__rows'  =>  $dbQuery->rowCount(), '__columns'  =>  $dbQuery->columnCount())) : $dbQuery->$fetchMethod();
+            $this->last_query = $dbQuery;
+            
+            return $dbQuery;
         }
-        
-        $fetchMethod = (isset($fetchType)) ? $fetchType : 'fetch';
-        $dbQuery = (!! $additional === true) ? array_merge($dbQuery->$fetchMethod(), array('__rows'  =>  $dbQuery->rowCount(), '__columns'  =>  $dbQuery->columnCount())) : $dbQuery->$fetchMethod();
-        $this->last_query = $dbQuery;
-        
-        return $dbQuery;
+        return false;
     }
     
-    function fetchsize()
+    function fetchSize()
     {
         if(isset($this->last_query['__rows']) && isset($this->last_query['__columns']))
         {
